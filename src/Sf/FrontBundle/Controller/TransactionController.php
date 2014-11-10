8286385b8f6291131988989e7c527f87ce60b278
Karudev <renault@karudev.fr>
2014-11-10 16:07:08 +0100

    up

--- src/Sf/FrontBundle/Controller/TransactionController.php
+++ src/Sf/FrontBundle/Controller/TransactionController.php
@@ -145,7 +145,7 @@ class TransactionController extends Controller {
 
         $transaction = $em->getRepository('SfAdminBundle:Transaction')->checkout(
                 array(
-                    'amountTtc' => $payment_amount,
+                    'amountHt' => $payment_amount - $tax,
                     'currency' => $payment_currency,
                     'tva' => $tax,
                     'shipping' => $shipping,
